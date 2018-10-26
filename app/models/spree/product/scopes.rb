894d9da60818b5c82adff9fb6d5645ae27cf0339
ramores <errands.ramores@gmail.com>
2018-10-26 16:56:28 +0800

    update 1026_bok_delete3

--- app/models/spree/product/scopes.rb
+++ app/models/spree/product/scopes.rb
@@ -75,86 +75,86 @@ module Spree
     #   vs.
     #
     #   SELECT COUNT(*) ...
-    # add_search_scope :in_taxon do |taxon|
-    #   includes(:classifications).
-    #     where('spree_products_taxons.taxon_id' => taxon.self_and_descendants.pluck(:id)).
-    #     order('spree_products_taxons.position ASC')
-    # end
+    add_search_scope :in_taxon do |taxon|
+      includes(:classifications).
+        where('spree_products_taxons.taxon_id' => taxon.self_and_descendants.pluck(:id)).
+        order('spree_products_taxons.position ASC')
+    end
+
+    # This scope selects products in all taxons AND all its descendants
+    # If you need products only within one taxon use
     #
-    # # This scope selects products in all taxons AND all its descendants
-    # # If you need products only within one taxon use
-    # #
-    # #   Spree::Product.taxons_id_eq([x,y])
-    # add_search_scope :in_taxons do |*taxons|
-    #   taxons = get_taxons(taxons)
-    #   taxons.first ? prepare_taxon_conditions(taxons) : where(nil)
-    # end
-    #
-    # # a scope that finds all products having property specified by name, object or id
-    # add_search_scope :with_property do |property|
-    #   joins(:properties).where(property_conditions(property))
-    # end
-    #
-    # # a simple test for product with a certain property-value pairing
-    # # note that it can test for properties with NULL values, but not for absent values
-    # add_search_scope :with_property_value do |property, value|
-    #   joins(:properties).
-    #     where("#{ProductProperty.table_name}.value = ?", value).
-    #     where(property_conditions(property))
-    # end
-    #
-    # add_search_scope :with_option do |option|
-    #   option_types = OptionType.table_name
-    #   conditions = case option
-    #                when String     then { "#{option_types}.name" => option }
-    #                when OptionType then { "#{option_types}.id" => option.id }
-    #                else { "#{option_types}.id" => option.to_i }
-    #   end
-    #
-    #   joins(:option_types).where(conditions)
-    # end
-    #
-    # add_search_scope :with_option_value do |option, value|
-    #   option_values = OptionValue.table_name
-    #   option_type_id = case option
-    #                    when String then OptionType.find_by(name: option) || option.to_i
-    #                    when OptionType then option.id
-    #                    else option.to_i
-    #   end
-    #
-    #   conditions = "#{option_values}.name = ? AND #{option_values}.option_type_id = ?", value, option_type_id
-    #   group('spree_products.id').joins(variants_including_master: :option_values).where(conditions)
-    # end
+    #   Spree::Product.taxons_id_eq([x,y])
+    add_search_scope :in_taxons do |*taxons|
+      taxons = get_taxons(taxons)
+      taxons.first ? prepare_taxon_conditions(taxons) : where(nil)
+    end
+
+    # a scope that finds all products having property specified by name, object or id
+    add_search_scope :with_property do |property|
+      joins(:properties).where(property_conditions(property))
+    end
+
+    # a simple test for product with a certain property-value pairing
+    # note that it can test for properties with NULL values, but not for absent values
+    add_search_scope :with_property_value do |property, value|
+      joins(:properties).
+        where("#{ProductProperty.table_name}.value = ?", value).
+        where(property_conditions(property))
+    end
+
+    add_search_scope :with_option do |option|
+      option_types = OptionType.table_name
+      conditions = case option
+                   when String     then { "#{option_types}.name" => option }
+                   when OptionType then { "#{option_types}.id" => option.id }
+                   else { "#{option_types}.id" => option.to_i }
+      end
+
+      joins(:option_types).where(conditions)
+    end
+
+    add_search_scope :with_option_value do |option, value|
+      option_values = OptionValue.table_name
+      option_type_id = case option
+                       when String then OptionType.find_by(name: option) || option.to_i
+                       when OptionType then option.id
+                       else option.to_i
+      end
+
+      conditions = "#{option_values}.name = ? AND #{option_values}.option_type_id = ?", value, option_type_id
+      group('spree_products.id').joins(variants_including_master: :option_values).where(conditions)
+    end
 
     # Finds all products which have either:
     # 1) have an option value with the name matching the one given
     # 2) have a product property with a value matching the one given
-    # add_search_scope :with do |value|
-    #   includes(variants_including_master: :option_values).
-    #     includes(:product_properties).
-    #     where("#{OptionValue.table_name}.name = ? OR #{ProductProperty.table_name}.value = ?", value, value)
-    # end
+    add_search_scope :with do |value|
+      includes(variants_including_master: :option_values).
+        includes(:product_properties).
+        where("#{OptionValue.table_name}.name = ? OR #{ProductProperty.table_name}.value = ?", value, value)
+    end
 
     # Finds all products that have a name containing the given words.
-    # add_search_scope :in_name do |words|
-    #   like_any([:name], prepare_words(words))
-    # end
+    add_search_scope :in_name do |words|
+      like_any([:name], prepare_words(words))
+    end
 
     # Finds all products that have a name or meta_keywords containing the given words.
-    # add_search_scope :in_name_or_keywords do |words|
-    #   like_any([:name, :meta_keywords], prepare_words(words))
-    # end
+    add_search_scope :in_name_or_keywords do |words|
+      like_any([:name, :meta_keywords], prepare_words(words))
+    end
 
     # Finds all products that have a name, description, meta_description or meta_keywords containing the given keywords.
-    # add_search_scope :in_name_or_description do |words|
-    #   like_any([:name, :meta_keywords], prepare_words(words))
-    # end
+    add_search_scope :in_name_or_description do |words|
+      like_any([:name, :meta_keywords], prepare_words(words))
+    end
 
     # Finds all products that have the ids matching the given collection of ids.
     # Alternatively, you could use find(collection_of_ids), but that would raise an exception if one product couldn't be found
-    # add_search_scope :with_ids do |*ids|
-    #   where(id: ids)
-    # end
+    add_search_scope :with_ids do |*ids|
+      where(id: ids)
+    end
 
     # Sorts products from most popular (popularity is extracted from how many
     # times use has put product in cart, not completed orders)
@@ -164,51 +164,51 @@ module Spree
     #
     # joins: "LEFT OUTER JOIN (SELECT line_items.variant_id as vid, COUNT(*) as cnt FROM line_items GROUP BY line_items.variant_id) AS popularity_count ON variants.id = vid",
     # order: 'COALESCE(cnt, 0) DESC'
-    # add_search_scope :descend_by_popularity do
-    #   joins(:master).
-    #     order(%Q{
-    #        COALESCE((
-    #          SELECT
-    #            COUNT(#{LineItem.quoted_table_name}.id)
-    #          FROM
-    #            #{LineItem.quoted_table_name}
-    #          JOIN
-    #            #{Variant.quoted_table_name} AS popular_variants
-    #          ON
-    #            popular_variants.id = #{LineItem.quoted_table_name}.variant_id
-    #          WHERE
-    #            popular_variants.product_id = #{Product.quoted_table_name}.id
-    #        ), 0) DESC
-    #     })
-    # end
-    #
-    # add_search_scope :not_deleted do
-    #   where("#{Product.quoted_table_name}.deleted_at IS NULL or #{Product.quoted_table_name}.deleted_at >= ?", Time.zone.now)
-    # end
-    #
-    # def self.not_discontinued(only_not_discontinued = true)
-    #   if only_not_discontinued != '0' && only_not_discontinued
-    #     where("#{Product.quoted_table_name}.discontinue_on IS NULL or #{Product.quoted_table_name}.discontinue_on >= ?", Time.zone.now)
-    #   else
-    #     all
-    #   end
-    # end
-    # search_scopes << :not_discontinued
-    # # Can't use add_search_scope for this as it needs a default argument
-    # def self.available(available_on = nil, _currency = nil)
-    #   available_on ||= Time.current
-    #   not_discontinued.joins(master: :prices).where("#{Product.quoted_table_name}.available_on <= ?", available_on)
-    # end
-    # search_scopes << :available
-    #
-    # def self.active(currency = nil)
-    #   available(nil, currency)
-    # end
-    # search_scopes << :active
-    #
-    # add_search_scope :taxons_name_eq do |name|
-    #   group('spree_products.id').joins(:taxons).where(Taxon.arel_table[:name].eq(name))
-    # end
+    add_search_scope :descend_by_popularity do
+      joins(:master).
+        order(%Q{
+           COALESCE((
+             SELECT
+               COUNT(#{LineItem.quoted_table_name}.id)
+             FROM
+               #{LineItem.quoted_table_name}
+             JOIN
+               #{Variant.quoted_table_name} AS popular_variants
+             ON
+               popular_variants.id = #{LineItem.quoted_table_name}.variant_id
+             WHERE
+               popular_variants.product_id = #{Product.quoted_table_name}.id
+           ), 0) DESC
+        })
+    end
+
+    add_search_scope :not_deleted do
+      where("#{Product.quoted_table_name}.deleted_at IS NULL or #{Product.quoted_table_name}.deleted_at >= ?", Time.zone.now)
+    end
+
+    def self.not_discontinued(only_not_discontinued = true)
+      if only_not_discontinued != '0' && only_not_discontinued
+        where("#{Product.quoted_table_name}.discontinue_on IS NULL or #{Product.quoted_table_name}.discontinue_on >= ?", Time.zone.now)
+      else
+        all
+      end
+    end
+    search_scopes << :not_discontinued
+    # Can't use add_search_scope for this as it needs a default argument
+    def self.available(available_on = nil, _currency = nil)
+      available_on ||= Time.current
+      not_discontinued.joins(master: :prices).where("#{Product.quoted_table_name}.available_on <= ?", available_on)
+    end
+    search_scopes << :available
+
+    def self.active(currency = nil)
+      available(nil, currency)
+    end
+    search_scopes << :active
+
+    add_search_scope :taxons_name_eq do |name|
+      group('spree_products.id').joins(:taxons).where(Taxon.arel_table[:name].eq(name))
+    end
 
     def self.distinct_by_product_ids(sort_order = nil)
       sort_column = sort_order.split(' ').first
