c3adaea2c79bbb379aa75950235456fa73f02646
ramores <errands.ramores@gmail.com>
2018-10-30 16:42:10 +0800

    update 1030v3

--- app/overrides/edit_search.rb
+++ app/overrides/edit_search.rb
@@ -2,7 +2,7 @@ Override.new(
   :virtual_path => "spree/shared/_search",
   :name => "set_button_to_green",
   :replace => "erb[loud]:contains('submit_tag')",
-  :text => "<button type="submit" class="btn btn-success" data-disable-with="Search">
-            <span class="glyphicon glyphicon-search"></span>
+  :text => "<button type='submit' class='btn btn-success' data-disable-with='Search'>
+            <span class='glyphicon glyphicon-search'></span>
             </button>"
 )
