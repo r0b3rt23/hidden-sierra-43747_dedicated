dd5ebb64896734ea06210803c27824b38713576d
ramores <errands.ramores@gmail.com>
2018-10-30 16:51:43 +0800

    update 1030v3

--- app/overrides/edit_search.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(
-  :virtual_path => "spree/shared/_search",
-  :name => "set_button_to_green",
-  :replace => "<%= submit_tag Spree.t(:search), name: nil, class: 'btn btn-success', id: 'btn-id' %>",
-  :text => "<button type='submit' class='btn btn-success' data-disable-with='Search'>
-            <span class='glyphicon glyphicon-search'></span>
-            </button>"
-)
