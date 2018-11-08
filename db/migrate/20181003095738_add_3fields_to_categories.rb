814ca0f0392aee1fe333520e0e5dc9c35246ccda
ramores <errands.ramores@gmail.com>
2018-11-08 10:01:19 +0800

    updated_1108

--- db/migrate/20181003095738_add_3fields_to_categories.rb
+++
@@ -1,7 +0,0 @@
-class Add3fieldsToCategories < ActiveRecord::Migration[5.1]
-  def change
-    add_column :categories, :cgroup, :integer
-    add_column :categories, :sort_order, :integer
-    add_column :categories, :active, :boolean
-  end
-end
